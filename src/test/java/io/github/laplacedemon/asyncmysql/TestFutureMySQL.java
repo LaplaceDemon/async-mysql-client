0315cf38db1a75bd49b822c6d5ec435d16008b3c
fanfan <fanfan@bogon>
2019-03-26 00:23:39 +0800

    Temporary submission

---
+++ src/test/java/io/github/laplacedemon/asyncmysql/TestFutureMySQL.java
@@ -0,0 +1,57 @@
+package io.github.laplacedemon.asyncmysql;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+
+import org.junit.Test;
+
+public class TestFutureMySQL {
+	/*
+	@Test
+	public void testAsyncConnect() throws IOException, InterruptedException {
+		final AsyncMySQL asyncMySQL = AsyncMySQL.create();
+		Config config = asyncMySQL.makeConfig("127.0.0.1", 3306,"root","shijiaqi1066");
+		asyncMySQL.connect(config)
+		.then((recever, Connection con)->{
+			con.executeQuery("select 1+1");
+		}).then((ResultSet r)-> {
+			
+		});
+		
+		asyncMySQL.start();
+	}
+	
+	@Test
+	public void testTxn() throws IOException, InterruptedException {
+		final AsyncMySQL asyncMySQL = AsyncMySQL.create();
+		Config config = asyncMySQL.makeConfig("127.0.0.1", 3306,"root","shijiaqi1066");
+		asyncMySQL.connect(config)
+		.then((Connection con) -> {
+			return con.startTxn();
+		}).then((Connection con) -> {
+			return con.executeUpdate("");
+		}).then((Connection con) -> {
+			return con.endTxn();
+		}).then(() -> {
+			System.out.println("结束");
+		});
+		
+		asyncMySQL.start();
+	}
+	
+	@Test
+	public void testTxn() throws IOException, InterruptedException {
+		final AsyncMySQL asyncMySQL = AsyncMySQL.create();
+		Config config = asyncMySQL.makeConfig("127.0.0.1", 3306,"root","shijiaqi1066");
+		asyncMySQL.connect(config)
+			.startTxn()
+			.executeUpdate("", (long count, long id)->{
+				
+			})
+			.executeUpdate("", ()->{})
+			.endTxn();
+		
+		asyncMySQL.start();
+	}
+	*/
+}
